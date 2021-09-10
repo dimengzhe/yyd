f49395e89edab02a78db8cd06f887c8ab24f7be5
dimengzhe <251008545@qq.com>
2021-09-10 16:17:41 +0800

    初始化构建前端框架

---
+++ yyd-login-ui/src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login"></div>
+</template>
+
+<style lang="scss">
+.login {
+  position: relative;
+  height: 100%;
+  background: url(~@/assets/images/login/bgimg.jpg) center no-repeat;
+  background-size: 100% 100%;
+}
+</style>
