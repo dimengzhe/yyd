8facd99fa4a4b3f872bd220045631092dc6b20ee
dimengzhe <251008545@qq.com>
2022-04-01 13:46:23 +0800

    测试

--- yyd-demo/yyd-demo-biz/src/main/java/com/yxt/yyd/demo/DemoApplication.java
+++ yyd-demo/yyd-demo-biz/src/main/java/com/yxt/yyd/demo/DemoApplication.java
@@ -18,6 +18,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 })
 public class DemoApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
