9be893a51bfa99cae94bc7efb386356f5c91a8d1
dimengzhe <251008545@qq.com>
2022-04-02 23:16:22 +0800

    创建jdbc模块

---
+++ yyd-common/yyd-common-jdbc/src/main/java/com/yxt/yyd/common/jdbc/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.yxt.yyd.common.jdbc;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dimengzhe
+ * @date 2021/9/4 0:40
+ * @description
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
