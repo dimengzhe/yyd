f34d2d918d906fc69fecc248635696a5b09984d4
dimengzhe <251008545@qq.com>
2022-07-24 00:08:44 +0800

    项目工具部分修整

---
+++ yyd-common/yyd-common-utils/src/main/java/com/yxt/yyd/common/utils/allutils/RegexpUtils.java
@@ -0,0 +1,12 @@
+package com.yxt.yyd.common.utils.allutils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author dimengzhe
+ * @Date 2022/7/23 23:59
+ * @Description
+ */
+@Slf4j
+public class RegexpUtils {
+}
